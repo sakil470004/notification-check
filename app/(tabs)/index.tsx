e015a28d8fc5c8329c82f0dfec592e3681972aab
Mynul Islam <mynul.sakil@gmail.com>
2025-05-15 03:18:28 +0600

    added notificaiton but now wroking

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,27 @@
 import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { Button, Platform, StyleSheet } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import * as Notifications from 'expo-notifications';
 
 export default function HomeScreen() {
+  function scheduleNotificationHandler() {
+    Notifications.scheduleNotificationAsync({
+      content:{
+        title: 'Hello! Mynul',
+        body: 'This is a test notification body',
+        data: { data: 'goes here',userName: 'Mynul' },
+        sound: 'default',
+      },
+      trigger: {
+        seconds: 10,
+      },
+
+    })
+  }
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
@@ -17,9 +32,22 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Welcome! Mynul</ThemedText>
         <HelloWave />
+        
+
       </ThemedView>
+      <ThemedText>
+        <Button
+          title="Get Notified"
+          onPress={scheduleNotificationHandler}
+          style={{
+            backgroundColor: '#007AFF',
+            padding: 10,
+            borderRadius: 5,
+          }}
+        > </Button>
+        </ThemedText>
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
         <ThemedText>
